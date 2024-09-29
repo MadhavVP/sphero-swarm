afb5bf7712e5936917f75bbc81425bc01d3e0b7c
Madhav <mvaliyap@purdue.edu>
2024-09-29 16:53:50 -0400

    Modified command structure to be more inuitive, added speed functionality to move commands

--- controls/Controls_SS.py
+++ controls/Controls_SS.py
@@ -14,7 +14,7 @@ def toy_manager(toy, id):
             global allReady
             global commands
 
-            choosenColor = Color(r = 0, g = 0, b = 0)
+            choosenColor = Color(r = 0, g = 255, b = 0)
             
             magBase = 0
             direction = 0   
@@ -24,46 +24,26 @@ def toy_manager(toy, id):
                 print("{}: {}".format(id, commands[id][0]))
                 if (commands[id][0] == "%"): # be very careful with this - needs to be the absolute last command!
                     return
-                elif (commands[id][0] == "c"):
+                elif (commands[id][0] == "CALIBRATE"):
                     api.calibrate_compass() # not perfect - need feedback
                     magBase = api.get_compass_direction()
                     api.set_main_led(choosenColor)
-                elif (commands[id][0][0] == "m"):
-                    api.roll(magBase + direction, 255, float(commands[id][0][1:]))
+                elif (commands[id][0][0] == "M"): #MOVE:XXX:YYY    - Moves in preset direction at XXX speed for YYY seconds
+                    api.roll(magBase + direction, int(commands[id][0][2:4]), float(commands[id][0][6:]))
                     time.sleep(0.5)
-                elif (commands[id][0][0] == "R"):
-                    direction += int(commands[id][0][1:])
+                elif (commands[id][0][0] == "R"): #ROTATE: R:XXX
+                    direction += int(commands[id][0][2:])
                     # probably need to graph sphero turning in order to get a good idea of what the rotate command should be
-                elif (commands[id][0] == "Cred"):
-                    choosenColor = Color(r = 255, g = 0, b = 0)
+                elif (commands[id][0][0] == "C"): #COLOR CHANGE: C:RRR:GGG:BBB    - sets color to these rgb vals
+                    choosenColor = Color(r = int(commands[id][0][2:4]), g = int(commands[id][0][6:8]), b = int(commands[id][0][10:12]))
                     api.set_main_led(choosenColor)
                     api.set_front_led(choosenColor)
                     api.set_back_led(choosenColor)
-                elif (commands[id][0] == "Cgreen"):
-                    choosenColor = Color(r = 0, g = 255, b = 0)
-                    api.set_main_led(choosenColor)
-                    api.set_front_led(choosenColor)
-                    api.set_back_led(choosenColor)
-                elif (commands[id][0] == "Cblue"):
-                    choosenColor = Color(r = 0, g = 0, b = 255)
-                    api.set_main_led(choosenColor)
-                    api.set_front_led(choosenColor)
-                    api.set_back_led(choosenColor)
-                elif (commands[id][0] == "Cblack"):
-                    choosenColor = Color(r = 0, g = 0, b = 0)
-                    api.set_main_led(choosenColor)
-                    api.set_front_led(choosenColor)
-                    api.set_back_led(choosenColor)
-                elif (commands[id][0] == "Cwhite"):
-                    choosenColor = Color(r = 255, g = 255, b = 255)
-                    api.set_main_led(choosenColor)
-                    api.set_front_led(choosenColor)
-                    api.set_back_led(choosenColor)
-                elif (commands[id][0][0] == "d"):
-                    time.sleep(float(commands[id][0][1:]))
+                elif (commands[id][0][0] == "D"):
+                    time.sleep(float(commands[id][0][2:]))
                 else:
                     print(commands[id][0] + " in ball {} is an invalid command!".format(id))
-                commands[id] = commands[id][1:]
+                commands[id].pop(0) #Pops the command that was just executed
                 allReady[id][numIterations] = 1
                 while True:
                     ready = True
@@ -85,7 +65,7 @@ def commandInputs(toys): # needs to be able to consistently take in data
 
     #commands = [[], [], []]
 
-    command =  ["Cblue", "m0.25", "R90", "m0.25", "R30", "d2", "m0.5", "%"]
+    command =  ["C:000:255:120", "M:155:0.25", "R:90", "M:255:0.25", "R:30", "D:2", "M:100:0.5", "%"]
     for toy in toys:
         commands.append(command) # matrix is needed for more complex commands
         allReady.append([0] * len(command))
